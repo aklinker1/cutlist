ba7a7339d45135faba18ed61b70a0208bfc09183
Aaron <aaronklinker1@gmail.com>
2024-03-18 23:56:37 -0500

    fix: Format part details better

--- web/components/PartDetails.vue
+++ web/components/PartDetails.vue
@@ -21,32 +21,39 @@ const bottom = useFormattedDistance(() => props.placement?.bottom);
     </h3>
     <table class="font-mono">
       <tr>
-        <th>W=</th>
+        <th class="opacity-50">Width</th>
+        <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
         <td>{{ width }}</td>
       </tr>
       <tr>
-        <th>L=</th>
+        <th class="opacity-50">Length</th>
+        <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
         <td>{{ length }}</td>
       </tr>
       <tr>
-        <th>T=</th>
+        <th class="opacity-50">Thick</th>
+        <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
         <td>{{ thickness }}</td>
       </tr>
       <template v-if="placement">
         <tr>
-          <th>LEFT=</th>
+          <th class="opacity-50">Left</th>
+          <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
           <td>{{ left }}</td>
         </tr>
         <tr>
-          <th>TOP=</th>
+          <th class="opacity-50">Top</th>
+          <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
           <td>{{ top }}</td>
         </tr>
         <tr>
-          <th>RIGHT=</th>
+          <th class="opacity-50">Right</th>
+          <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
           <td>{{ right }}</td>
         </tr>
         <tr>
-          <th>BOTTOM=</th>
+          <th class="opacity-50">Bottom</th>
+          <td class="opacity-50 whitespace-pre">{{ ' = ' }}</td>
           <td>{{ bottom }}</td>
         </tr>
       </template>
