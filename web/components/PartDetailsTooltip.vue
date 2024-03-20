b3f4dd8b72138ebdf44b40cc064a2a7369cdcd3d
Aaron <aaronklinker1@gmail.com>
2024-03-20 00:27:23 -0500

    fix: Reduce mouse listeners

--- web/components/PartDetailsTooltip.vue
+++ web/components/PartDetailsTooltip.vue
@@ -3,11 +3,15 @@ const props = defineProps<{
   part: Rectangle<PartToCut>;
 }>();
 
-const { x, y } = useMouse();
+const { x, y } = useGlobalMouse();
 </script>
 
 <template>
-  <div class="fixed w-px h-px z-10" :style="`left:${x}px;top:${y}px`">
+  <div
+    v-if="x != 0 && y != 0"
+    class="fixed w-px h-px z-10"
+    :style="`left:${x}px;top:${y}px`"
+  >
     <PartDetails
       class="translate-x-[-50%] translate-y-8 p-2 bg-gray-600 rounded shadow-xl min-w-[256px]"
       :part="part.data"
